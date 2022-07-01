54e23e40ec6361443b06f05efbcdb5b377128213
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-06-30 21:31:07 -0300

    Update RestaurantRepository.java

--- src/main/java/com/tuyo/tuyofood/domain/repository/RestaurantRepository.java
+++ src/main/java/com/tuyo/tuyofood/domain/repository/RestaurantRepository.java
@@ -2,7 +2,9 @@ package com.tuyo.tuyofood.domain.repository;
 
 import com.tuyo.tuyofood.domain.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
 }
